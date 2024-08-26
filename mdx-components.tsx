5ea0308c34329b6289985648389a1cbb5ca7f245
Vaibhav <vaibhav.pandey0806@gmail.com>
2024-08-26 13:55:09 +0530

    add: quickstart page

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,11 @@
 import type { MDXComponents } from 'mdx/types';
 import defaultComponents from 'fumadocs-ui/mdx';
+import { Icon } from '@iconify/react'; // Import the entire Iconify library.
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...defaultComponents,
     ...components,
+    IIcon: Icon,
   };
 }
