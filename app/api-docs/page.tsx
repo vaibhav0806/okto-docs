148f9ee31f012f500eedd03034345dfd621cc2ea
Vaibhav <vaibhav.pandey0806@gmail.com>
2024-10-17 09:30:12 +0530

    update: swagger url

--- app/api-docs/page.tsx
+++ app/api-docs/page.tsx
@@ -43,7 +43,7 @@ const App = () => {
       <ApiReferenceReact
         configuration={{
           spec: {
-            url: 'https://docs.okto.tech/api-sec',
+            url: 'https://okto-docs-black.vercel.app/api-sec',
           },
           theme: 'none',
           darkMode: theme === 'dark',
